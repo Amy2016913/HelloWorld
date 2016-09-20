534831d20d8cd2d7fd5128d7c4fdd92ce451117f
mxw <1563672996@qq.com>
2016-09-20 15:08:35 +0800

    Finish Homework 2

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-var a, b, c: number;
-var starstring, spacestring: string;
-
-var x: string = process.argv[2];
-
-var num: number = parseInt(x);
-
+var a, b, c;
+var starstring, spacestring;
+var x = process.argv[2];
+var num = parseInt(x);
 for (a = 0; a < num; a++) {
     starstring = "";
     spacestring = "";
-
     for (b = 0; b < (num - a - 1); b++) {
         spacestring += " ";
     }
-
     for (c = 0; c < (2 * a + 1); c++) {
         starstring += "*";
     }
-
     console.log(spacestring + starstring);
 }
