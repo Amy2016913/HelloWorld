8cabe3d2a8cd9782217c308c1250d948b79120e8
Amy2016913 <1563672996@qq.com>
2016-09-22 17:32:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var a, b, c;
+var maxline, spacenum, starnum;
 var starstring, spacestring;
 var x = process.argv[2];
 var num = parseInt(x);
-for (a = 0; a < num; a++) {
+for (maxline = 0; maxline < num; maxline++) {
     starstring = "";
     spacestring = "";
-    for (b = 0; b < (num - a - 1); b++) {
+    for (spacenum = 0; spacenum < (num - maxline - 1); spacenum++) {
         spacestring += " ";
     }
-    for (c = 0; c < (2 * a + 1); c++) {
+    for (starnum = 0; starnum < (2 * maxline + 1); starnum++) {
         starstring += "*";
     }
     console.log(spacestring + starstring);
